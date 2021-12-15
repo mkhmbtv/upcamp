2118128ae46f3b3ddd4646e975c126db0caa215f
your name here <mmakhambetov@yandex.ru>
2021-12-16 01:40:09 +0400

    Set up a User model

---
+++ backend/db/models/user.js
@@ -0,0 +1,51 @@
+'use strict';
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50],
+      },
+    },
+    lastName: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50],
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Username cannot be an email.')
+          }
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 256],
+      },
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60],
+      },
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
---
+++ backend/db/seeders/20211215211804-demo-user.js
@@ -0,0 +1,35 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+const faker = require('faker');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Demo',
+        lastName: 'User',
+        username: 'Demo-user',
+        email: 'demo@user.io',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      }
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
