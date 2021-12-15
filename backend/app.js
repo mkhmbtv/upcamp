bb3b19cd264143cc47ad37d83feaf286937d52bb
your name here <mmakhambetov@yandex.ru>
2021-12-16 00:39:56 +0400

    Add error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csrf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -31,5 +32,35 @@ app.use(csrf({
 }));
 
 app.use(routes);
+
+// Catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => {
+  const err = new Error('The requested resource could not be found.');
+  err.title = 'Resource Not Found';
+  err.errors = ['The requested resource could not be found.'];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((error) => error.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
