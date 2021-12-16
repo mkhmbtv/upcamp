0b7df5c94a08b7128b4f7c70e7f8eac77fef13bb
your name here <mmakhambetov@yandex.ru>
2021-12-16 21:17:11 +0400

    Create User signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,33 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { User } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+
 const router = express.Router();
+
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { 
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    } = req.body;
+
+    const user = await User.signup({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    });
+
+    await setTokenCookie(res, user);
+    res.json({ user });
+  }),
+);
 
 module.exports = router;
