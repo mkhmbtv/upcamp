78945805df34fb97cfee2c6cb892a26776dba728
your name here <mmakhambetov@yandex.ru>
2021-12-19 15:52:03 +0400

    Style LoginForm

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, useNavigate } from 'react-router';
+import { Navigate, useNavigate, Link } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
@@ -10,13 +11,14 @@ const LoginForm = () => {
   const [credential, setCredential] = useState('');
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
-
+  
   if (sessionUser) return (
     <Navigate replace to='/' />
   )
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setErrors([]);
     return dispatch(login({ credential, password }))
       .then(() => navigate('/'))
       .catch(async (res) => {
@@ -25,43 +27,53 @@ const LoginForm = () => {
       })
   };
 
+  const handleDemoLogin = () => {
+    setErrors([]);
+    setCredential('demo@user.io');
+    setPassword('password');
+  }
+
   return (
-    <div>
-      <h2>
-        Welcome Back!
-        <br />
-        <small>Let's get you outside</small>
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((err, i) => (
-            <li key={i}>{err}</li>
-          ))}
-        </ul>
-        <div>
-          <label>
-            Username or Email
-            <input
-              type='text'
-              onChange={(e) => setCredential(e.target.value)}
-              value={credential}
-              required
-            />
-          </label>
+    <div className='loginForm'>
+      <div className='loginForm__container'>
+        <h2 className='loginForm__header'>
+          Welcome back!
+          <br />
+          <small className='loginForm__header--sub'>Let's get you outside</small>
+        </h2>
+        <button className='loginForm__btn loginForm__btn--demo' onClick={handleDemoLogin}>Demo User</button>
+        <div className='loginForm__strike'>
+          or
         </div>
-        <div>
-          <label>
-            Password
-            <input
-              type='password'
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              required
-            />
-          </label>
-        </div>
-        <button type='submit'>Log In</button>
-      </form>
+        <form className='loginForm__form' onSubmit={handleSubmit}>
+          <ul className='loginForm__errors'>
+            {errors.map((err, i) => (
+              <li key={i} className='loginForm__error'>{err}</li>
+            ))}
+          </ul>
+          <input
+            className='loginForm__input'
+            type='text'
+            onChange={(e) => setCredential(e.target.value)}
+            value={credential}
+            placeholder='Username or Email Adress'
+            required
+          />
+          <input
+            className='loginForm__input'
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder='Password'
+            required
+          />
+          <button className='loginForm__btn' type='submit'>Log In</button>
+        </form>
+      </div>
+      <div className='loginForm__footer'>
+        Don't have an Upcamp account?
+        <Link to='/signup' className='loginForm__link'>Sign Up</Link>
+      </div>
     </div>
   )
 };
